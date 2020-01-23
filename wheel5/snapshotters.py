530e27c83cb8a5e570973cc81ff7dd3672ce4836
Alexander Demidko <alexander@fluence.one>
2020-01-23 11:02:12 +0300

    Refactored snapshotters, added a way to list/load related snapshots

--- wheel5/snapshotters.py
+++ wheel5/snapshotters.py
@@ -44,10 +44,10 @@ class FitState(object):
         torch.save(d, path)
 
     @staticmethod
-    def load(path: str, device: Union[torch.device, int]) -> 'FitState':
+    def load(path: str) -> 'FitState':
         d = torch.load(path)
         return FitState(
-            model=d['model'].to(device),
+            model=d['model'],
             loss=d['loss'],
             optimizer=d['optimizer'],
             epoch=d['epoch'],
@@ -58,37 +58,69 @@ class FitState(object):
 
 
 class Snapshotter(ABC):
-    @abstractmethod
-    def epoch_completed(self, state: FitState):
-        pass
-
-
-class BestCVSnapshotter(Snapshotter):
-    def __init__(self, dump_dir: str, metric_name: str, asc: bool, best: int = 1):
-        self.logger = logging.getLogger(f'{__name__}.BestCVSnapshotter')
-
-        self.metric_name = metric_name
-        self.ascending = asc
-        self.safe_metric_name = re.sub(r'[^0-9a-zA-Z]+', '', self.metric_name)
-        self.best = best
+    def __init__(self, dump_dir: str):
+        self.logger = logging.getLogger(f'{__name__}.{self.__class__.__name__}')
 
         self.dump_dir = dump_dir
         pathlib.Path(dump_dir).mkdir(parents=True, exist_ok=True)
         self.logger.info(f'Initialized snapshot dir: {dump_dir}')
 
-        entries = [entry for entry in os.listdir(dump_dir) if os.path.isfile(entry)]
-        pattern = re.compile(r'^bestcv-epoch_(?P<epoch>\d+)-' + self.safe_metric_name + r'_(?P<metric>[-+]?[0-9]*\.?[0-9]+)\.pth\.tar$')
-        matched_entries = []
-        for filename in entries:
-            m = pattern.match(filename)
-            if m is not None:
-                matched_entries.append({
-                    'epoch': m.group('epoch'),
-                    'metric': m.group('metric'),
-                    'filename': filename
-                })
+    @abstractmethod
+    def epoch_completed(self, state: FitState):
+        pass
 
-        self.leaderboard = pd.DataFrame(matched_entries, columns=['epoch', 'metric', 'filename'])
+    @abstractmethod
+    def list_snapshots(self) -> pd.DataFrame:
+        pass
+
+    def save_snapshot(self, filename: str, state: FitState):
+        path = os.path.join(self.dump_dir, filename)
+        FitState.save(path, state)
+        self.logger.info(f'Saved snapshot: {path}')
+
+    def load_snapshot(self, filename: str) -> FitState:
+        path = os.path.join(self.dump_dir, filename)
+        state = FitState.load(path)
+        self.logger.debug(f'Loaded snapshot: {path}')
+        return state
+
+    def drop_snapshot(self, filename: str):
+        path = os.path.join(self.dump_dir, filename)
+        os.remove(path)
+        self.logger.info(f'Dropped snapshot: {path}')
+
+    def _list_snapshots(self, pattern: re.Pattern):
+        entries = [entry for entry in os.listdir(self.dump_dir) if os.path.isfile(entry)]
+
+        data = []
+        for entry in entries:
+            m = pattern.match(entry)
+            if m is not None:
+                row = m.groupdict()
+                assert 'filename' not in row
+
+                row['filename'] = entry
+                data.append(row)
+
+        columns = list(pattern.groupindex.keys())
+        return pd.DataFrame(data, columns=columns)
+
+
+class BestCVSnapshotter(Snapshotter):
+    def __init__(self, dump_dir: str, metric_name: str, asc: bool, best: int = 1):
+        super().__init__(dump_dir)
+        assert best >= 1
+
+        self.metric_name = metric_name
+        self.ascending = asc
+        self.best = best
+
+        metric_name_pfx = re.sub(r'[^0-9a-zA-Z]+', '', self.metric_name)
+        asc_pfx = 'asc' if asc else 'desc'
+        self.prefix = f'bestcv-{metric_name_pfx}_{asc_pfx}'
+        self.pattern = re.compile(r'^' + self.prefix + r'-epoch_(?P<epoch>\d+)-(?P<metric>[-+]?[0-9]*\.?[0-9]+)\.pth\.tar$')
+
+        self.leaderboard = self.list_snapshots()
         self.leaderboard = self.leaderboard.sort_values(by='metric', ascending=self.ascending)
 
         leaderboard_dump = tabulate(self.leaderboard, headers="keys", showindex=False, tablefmt='github')
@@ -97,7 +129,7 @@ class BestCVSnapshotter(Snapshotter):
     def epoch_completed(self, state: FitState):
         epoch = int(state.epoch)
         metric = float(state.val_metrics[self.metric_name])
-        filename = f'bestcv-epoch_{epoch}-{self.safe_metric_name}_{metric:.8f}.pth.tar'
+        filename = f'{self.prefix}-epoch_{epoch}-{metric:.8f}.pth.tar'
 
         self.leaderboard = self.leaderboard.append({'epoch': epoch, 'metric': metric, 'filename': filename}, ignore_index=True)
         self.leaderboard = self.leaderboard.sort_values(by='metric', ascending=self.ascending)
@@ -109,47 +141,72 @@ class BestCVSnapshotter(Snapshotter):
         self.leaderboard = self.leaderboard[:self.best]
 
         for row in dropouts.itertuples():
-            path = os.path.join(self.dump_dir, row.filename)
-
-            if row.filename == filename:
-                self.logger.debug(f'Unqualified snapshot, no action: {path}')
-            else:
-                os.remove(path)
-                self.logger.info(f'Deleted snapshot: {path}')
+            if row.filename != filename:
+                self.drop_snapshot(row.filename)
 
         if (self.leaderboard['filename'] == filename).any():
-            path = os.path.join(self.dump_dir, filename)
-            FitState.save(path, state)
-            self.logger.info(f'Saved snapshot: {path}')
+            self.save_snapshot(filename, state)
+
+    def list_snapshots(self) -> pd.DataFrame:
+        return self._list_snapshots(self.pattern)
+
+
+class CheckpointSnapshotter(Snapshotter):
+    def __init__(self, dump_dir: str, frequency: int = 10):
+        super().__init__(dump_dir)
+        assert frequency >= 1
+
+        self.frequency = frequency
+
+        self.prefix = f'checkpoint'
+        self.pattern = re.compile(r'^' + self.prefix + r'-epoch_(?P<epoch>\d+)\.pth\.tar$')
+
+        self.last_filename = None
+
+    def epoch_completed(self, state: FitState):
+        if ((state.epoch - 1) % self.frequency == 0) or (state.epoch == state.num_epochs):
+            filename = f'{self.prefix}-epoch_{state.epoch}.pth.tar'
+            self.save_snapshot(filename, state)
+
+            if self.last_filename:
+                self.drop_snapshot(self.last_filename)
+
+            self.last_filename = filename
+
+    def list_snapshots(self) -> pd.DataFrame:
+        return self._list_snapshots(self.pattern)
 
 
 class PeriodicSnapshotter(Snapshotter):
     def __init__(self, dump_dir: str, frequency: int = 10):
-        self.logger = logging.getLogger(f'{__name__}.PeriodicSnapshotter')
+        super().__init__(dump_dir)
+        assert frequency >= 1
 
         self.frequency = frequency
 
-        self.dump_dir = dump_dir
-        pathlib.Path(dump_dir).mkdir(parents=True, exist_ok=True)
-        self.logger.info(f'Initialized snapshot dir: {dump_dir}')
+        self.prefix = f'periodic'
+        self.pattern = re.compile(r'^' + self.prefix + r'-epoch_(?P<epoch>\d+)\.pth\.tar$')
 
     def epoch_completed(self, state: FitState):
         if (state.epoch - 1) % self.frequency == 0:
-            path = os.path.join(self.dump_dir, f'periodic-epoch_{state.epoch}.pth.tar')
-            FitState.save(path, state)
-            self.logger.info(f'Saved snapshot: {path}')
+            filename = f'{self.prefix}-epoch_{state.epoch}.pth.tar'
+            self.save_snapshot(filename, state)
+
+    def list_snapshots(self) -> pd.DataFrame:
+        return self._list_snapshots(self.pattern)
 
 
 class FinalSnapshotter(Snapshotter):
     def __init__(self, dump_dir: str):
-        self.logger = logging.getLogger(f'{__name__}.FinalSnapshotter')
+        super().__init__(dump_dir)
 
-        self.dump_dir = dump_dir
-        pathlib.Path(dump_dir).mkdir(parents=True, exist_ok=True)
-        self.logger.info(f'Initialized snapshot dir: {dump_dir}')
+        self.prefix = f'final'
+        self.pattern = re.compile(r'^' + self.prefix + r'-epoch_(?P<epoch>\d+)\.pth\.tar$')
 
     def epoch_completed(self, state: FitState):
         if state.epoch == state.num_epochs:
-            path = os.path.join(self.dump_dir, f'final-epoch_{state.epoch}.pth.tar')
-            FitState.save(path, state)
-            self.logger.info(f'Saved snapshot: {path}')
+            filename = f'{self.prefix}-epoch_{state.epoch}.pth.tar'
+            self.save_snapshot(filename, state)
+
+    def list_snapshots(self) -> pd.DataFrame:
+        return self._list_snapshots(self.pattern)
