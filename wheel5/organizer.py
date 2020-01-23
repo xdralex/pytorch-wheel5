8f7c6a8391594c34b51575178f03d3861229d0ef
Alexander Demidko <alexander@fluence.one>
2020-01-23 12:27:57 +0300

    Better hyperparameters formatting for organizer trials

--- wheel5/organizer.py
+++ wheel5/organizer.py
@@ -1,3 +1,4 @@
+import math
 import datetime
 import os
 from typing import Optional, Dict, Union
@@ -32,10 +33,17 @@ class Organizer(object):
         self.trials = {}
 
     def trial(self, hparams: Optional[Dict[str, Union[int, float]]] = None):
+        def format_float(v: float) -> str:
+            zeros = math.ceil(math.log10(v))
+            if zeros < 5:
+                return f'{v:.{5 - zeros}f}'
+            else:
+                return f'{v:.1f}'
+
         if hparams is None:
             key = f'{datetime.datetime.now():%Y-%m-%d_%H:%M:%S}'
         else:
-            key = '-'.join([f'{k}_{v}' for k, v in hparams.items()])
+            key = '-'.join([f'{k}_{format_float(v)}' for k, v in hparams.items()])
 
         counter = self.trials.setdefault(key, 0)
         self.trials[key] += 1
