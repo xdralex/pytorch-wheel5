36f66e6d37d35a44b718abd97052e04d418db374
Alexander Demidko <alexander@fluence.one>
2020-01-30 18:52:15 +0300

    PadToSquare transform

--- wheel5/transforms.py
+++ wheel5/transforms.py
@@ -1,9 +1,28 @@
-import math
 from numbers import Number
 from typing import Union, Tuple
 
 from PIL import Image
 from torchvision.transforms import functional as F
+
+
+class PadToSquare(object):
+    def __init__(self, fill: Union[Number, str, tuple] = 0):
+        self.fill = fill
+
+    def __call__(self, img: Image) -> Image:
+        w, h = img.size
+        if w != h:
+            diff = max(w, h) - min(w, h)
+            d1 = int(diff / 2)
+            d2 = diff - d1
+
+            padding = (0, d1, 0, d2) if w > h else (d1, 0, d2, 0)
+            img = F.pad(img, padding, self.fill)
+
+        return img
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(fill={self.fill})'
 
 
 class SquarePaddedResize(object):
