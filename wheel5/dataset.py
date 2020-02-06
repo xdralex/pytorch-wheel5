6a3e60157d4c8ca8615867b7861245c291a67d3d
Alexander Demidko <alexander@fluence.one>
2020-02-06 19:27:59 +0300

    Refactored LMDBImageDataset

--- wheel5/dataset.py
+++ wheel5/dataset.py
@@ -1,7 +1,7 @@
 import os
 import pathlib
 from struct import pack, unpack
-from typing import Callable, Tuple, List, Optional, NamedTuple
+from typing import Callable, Tuple, List, Optional, NamedTuple, Any
 
 import albumentations as albu
 import lmdb
@@ -10,24 +10,55 @@ import pandas as pd
 from PIL import Image
 from PIL.Image import Image as Img
 from numpy.random.mtrand import RandomState
-from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
+from torch.utils.data import Dataset, DataLoader
 
 
-# TODO: make abstract enough to handle different datasets
-# TODO: use transform repr (name or hash) in the path to avoid clashing mistakes?
 class LMDBImageDataset(Dataset):
+    r"""A dataset storing images in the LMDB store.
+
+    This dataset takes a user-provided dataframe ['path', 'target', ...] to
+    locate the images. The 'path' column in the dataframe must point to
+    the image filename, and the 'target' column must contain the target value.
+    """
+
     @staticmethod
-    def cached(df: pd.DataFrame, image_dir: str, lmdb_path: str, lmdb_map_size: int = int(8 * (1024 ** 3)), transform: Callable[[Img], Img] = None):
+    def cached(df: pd.DataFrame,
+               image_dir: str,
+               lmdb_path: str,
+               lmdb_map_size: int = int(8 * (1024 ** 3)),
+               transform: Callable[[Img], Img] = None,
+               check_transform: bool = True):
+
         if os.path.exists(lmdb_path):
-            assert os.path.isdir(lmdb_path)
+            if not os.path.isdir(lmdb_path):
+                raise Exception(f'LMDB path {lmdb_path} must be a directory')
+
+            if check_transform:
+                with open(os.path.join(lmdb_path, '.transform'), mode='r') as f:
+                    lmdb_transform_str = f.read().strip()
+                    if lmdb_transform_str != str(transform):
+                        raise Exception(f'Transform used in LMDB is different from the supplied transform:\n' +
+                                        f'lmdb transform: {lmdb_transform_str}\n' +
+                                        f'supplied transform: {transform}')
         else:
             LMDBImageDataset.prepare(df, image_dir, lmdb_path, lmdb_map_size, transform)
 
         return LMDBImageDataset(df, lmdb_path, lmdb_map_size)
 
     @staticmethod
-    def prepare(df: pd.DataFrame, image_dir: str, lmdb_path: str, lmdb_map_size: int = int(8 * (1024 ** 3)), transform: Callable[[Img], Img] = None):
+    def prepare(df: pd.DataFrame,
+                image_dir: str,
+                lmdb_path: str,
+                lmdb_map_size: int = int(8 * (1024 ** 3)),
+                transform: Callable[[Img], Img] = None,
+                write_transform: bool = True):
+
         pathlib.Path(lmdb_path).mkdir(parents=True, exist_ok=False)
+
+        if write_transform:
+            with open(os.path.join(lmdb_path, '.transform'), mode='x') as f:
+                f.write(f'{transform}\n')
+
         with lmdb.open(lmdb_path, map_size=lmdb_map_size, subdir=True) as lmdb_env:
             with lmdb_env.begin(write=True) as txn:
                 for row in df.itertuples():
@@ -47,26 +78,21 @@ class LMDBImageDataset(Dataset):
                     v_meta = pack('HH', w, h)
                     txn.put(k_meta, v_meta)
 
-    def __init__(self, df: pd.DataFrame, lmdb_path: str, lmdb_map_size: int = int(8 * (1024 ** 3))):
+    def __init__(self,
+                 df: pd.DataFrame,
+                 lmdb_path: str,
+                 lmdb_map_size: int = int(8 * (1024 ** 3))):
         super(LMDBImageDataset, self).__init__()
 
         self.df = df
-
-        self.df_count = df.shape[0]
-
-        # TODO: bad naming
-        self.name_by_idx = list(np.sort(df['name'].unique()))
-        self.idx_by_name = {x: i for i, x in enumerate(self.name_by_idx)}
-
         self.lmdb_env = lmdb.open(lmdb_path, readonly=True, lock=False, meminit=False, readahead=False, map_size=lmdb_map_size, subdir=True)
 
     def __len__(self) -> int:
-        return self.df_count
+        return self.df.shape[0]
 
-    def __getitem__(self, index: int) -> Tuple[Img, int, str, int]:
+    def __getitem__(self, index: int) -> Tuple[Img, Any, int]:
         row = self.df.iloc[index, :]
-
-        name = row['name']
+        target = row['target']
 
         with self.lmdb_env.begin(write=False) as txn:
             k_data = f'data_{row.path}'.encode('ascii')
@@ -78,10 +104,7 @@ class LMDBImageDataset(Dataset):
             w, h = unpack('HH', v_meta)
             image = Image.frombytes('RGB', (w, h), v_data)
 
-        return image, self.idx_by_name[name], name, index
-
-    def classes(self) -> int:
-        return len(self.name_by_idx)
+        return image, target, index
 
 
 class TransformDataset(Dataset):
@@ -115,18 +138,6 @@ class AlbumentationsDataset(TransformDataset):
         super(AlbumentationsDataset, self).__init__(wrapped, callable_transform)
 
 
-class MappingDataset(Dataset):
-    def __init__(self, wrapped: Dataset, fn):
-        self.wrapped = wrapped
-        self.fn = fn
-
-    def __len__(self) -> int:
-        return len(self.wrapped)
-
-    def __getitem__(self, index: int):
-        return self.fn(self.wrapped[index])
-
-
 class DataBundle(NamedTuple):
     loader: DataLoader
     dataset: Dataset
@@ -142,28 +153,3 @@ def split_indices(indices: List[int], split: float, random_state: Optional[Rando
 
     divider = int(np.round(split * len(indices)))
     return shuffled_indices[:divider], shuffled_indices[divider:]
-
-
-def split_eval_main_data(bundle: DataBundle,
-                         split: float,
-                         eval_batch: int = 256,
-                         main_batch: int = 64,
-                         eval_workers: int = 4,
-                         main_workers: int = 4,
-                         random_state: Optional[RandomState] = None) -> (DataBundle, DataBundle):
-
-    if random_state is None:
-        random_state = np.random.RandomState()
-
-    eval_indices, main_indices = split_indices(bundle.indices, split=split, random_state=random_state)
-
-    eval_sampler = SubsetRandomSampler(eval_indices)
-    main_sampler = SubsetRandomSampler(main_indices)
-
-    eval_loader = DataLoader(bundle.dataset, batch_size=eval_batch, sampler=eval_sampler, num_workers=eval_workers, pin_memory=True)
-    main_loader = DataLoader(bundle.dataset, batch_size=main_batch, sampler=main_sampler, num_workers=main_workers, pin_memory=True)
-
-    eval_bundle = DataBundle(loader=eval_loader, dataset=bundle.dataset, indices=eval_indices)
-    main_bundle = DataBundle(loader=main_loader, dataset=bundle.dataset, indices=main_indices)
-
-    return eval_bundle, main_bundle
