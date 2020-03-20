4b28e7506e8ef23381f2af993e699035f16ad94c
Alexander Demidko <alexander@fluence.one>
2020-03-20 11:37:07 +0300

    Fixed class distribution computation

--- wheel5/dataset/functional.py
+++ wheel5/dataset/functional.py
@@ -8,10 +8,12 @@ from torch import Tensor
 
 def class_distribution(targets: List[int], classes: int) -> np.ndarray:
     counts = np.zeros(classes)
+
     for target in targets:
         assert 0 <= target < classes
         counts[target] += 1
 
+    counts = counts / float(len(targets))
     return counts
 
 
