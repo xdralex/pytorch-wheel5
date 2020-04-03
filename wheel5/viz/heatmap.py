a76e6977ebd14c55ccca157d64eba7c622328078
Alexander Demidko <alexander@fluence.one>
2020-04-03 18:57:14 +0300

    More flexible heatmap visualization

--- wheel5/viz/heatmap.py
+++ wheel5/viz/heatmap.py
@@ -1,72 +1,97 @@
+from dataclasses import dataclass
+from typing import List
+
+from abc import ABC, abstractmethod
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 from torchvision.transforms import functional as VTF
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from matplotlib.figure import Figure
-from numpy.random.mtrand import RandomState
-from sklearn.metrics import confusion_matrix
-from torch.utils.data import Dataset
-from typing import List
-import cv2
+
+class HeatmapMode(ABC):
+    @abstractmethod
+    def combine(self, image: torch.Tensor, mask: torch.Tensor):
+        pass
+
+
+@dataclass
+class HeatmapEntry:
+    name: str
+    y_class: torch.Tensor
+    mask: torch.Tensor
+    mode: HeatmapMode
+
+
+class HeatmapModeColormap(HeatmapMode):
+    def __init__(self, alpha: float = 0.5, colormap: int = cv2.COLORMAP_JET):
+        self.alpha = alpha
+        self.colormap = colormap
+
+    def combine(self, image: torch.Tensor, mask: torch.Tensor):
+        # image - shape: (3, H, W)
+        # mask - shape: (H, W)
+
+        heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)     # shape: (H, W, 3), BGR
+        heatmap = torch.from_numpy(heatmap).permute(2, 0, 1).float() / 255      # shape: (3, H, W), BGR
+        heatmap = heatmap[[2, 1, 0]]                                            # shape: (3, H, W), RGB
+
+        return image * self.alpha + heatmap * (1 - self.alpha)
+
+
+class HeatmapModeBloom(HeatmapMode):
+    def combine(self, image: torch.Tensor, mask: torch.Tensor):
+        # image - shape: (3, H, W)
+        # mask - shape: (H, W)
+
+        return torch.stack([image[i] * mask for i in range(0, image.shape[0])])
 
 
 def draw_heatmap(x: torch.Tensor,
-                 y_class: torch.Tensor,
-                 y_class_hat: torch.Tensor,
-                 mask: torch.Tensor,
-                 mask_hat: torch.Tensor,
+                 entries: List[HeatmapEntry],
                  target_classes: List[str],
+                 orientation: str = 'vertical',
+                 size: float = 5,
                  fontsize: int = 9,
                  alpha: float = 0.5):
 
-    def convert_mask(m: torch.Tensor):
-        hm = cv2.applyColorMap(np.uint8(255 * m), cv2.COLORMAP_JET)     # shape: (H, W, 3), BGR
-        hm = torch.from_numpy(hm).permute(2, 0, 1).float() / 255        # shape: (3, H, W), BGR
-        hm = hm[[2, 1, 0]]                                              # shape: (3, H, W), RGB
-        return hm
-
     # x - shape: (N, 3, H, W)
     # y_class - shape: (N)
-    # y_class_hat - shape: (N)
     # mask - shape: (N, H, W)
-    # mask_hat - shape: (N, H, W)
 
     assert len(x.shape) == 4
-    assert len(y_class.shape) == 1
-    assert len(y_class_hat.shape) == 1
-    assert len(mask.shape) == 3
-    assert len(mask_hat.shape) == 3
-
     assert x.shape[1] == 3
-    assert x.shape[2:] == mask.shape[1:]
-    assert x.shape[2:] == mask_hat.shape[1:]
 
-    for tensor in [y_class, y_class_hat, mask, mask_hat]:
-        assert x.shape[0] == tensor.shape[0]
+    for entry in entries:
+        assert len(entry.y_class.shape) == 1
+        assert len(entry.mask.shape) == 3
+
+        assert x.shape[0] == entry.y_class.shape[0]
+        assert x.shape[0] == entry.mask.shape[0]
+
+        assert x.shape[2:] == entry.mask.shape[1:]
 
     n, _, h, w = x.shape
 
-    fig = plt.figure(figsize=(5*2, 5*n))
+    if orientation == 'vertical':
+        cols, rows = len(entries), n
+    elif orientation == 'horizontal':
+        cols, rows = n, len(entries)
+    else:
+        raise AssertionError(f'Invalid orientation: {orientation}')
+
+    fig = plt.figure(figsize=(size * cols, size * rows))
 
     for i in range(0, n):
-        heatmap = convert_mask(mask[i])
-        heatmap_hat = convert_mask(mask_hat[i])
+        for j in range(0, len(entries)):
+            entry = entries[j]
 
-        plt.subplot(n, 2, 2*i + 1)
-        plt.imshow(VTF.to_pil_image(x[i]))
-        plt.imshow(VTF.to_pil_image(heatmap), alpha=alpha)
-        plt.title(f'actual: {target_classes[int(y_class[i])]}', fontsize=fontsize)
-        plt.grid(False)
-        plt.axis('off')
+            plt.subplot(rows, cols, len(entries) * i + j + 1)
+            plt.title(f'{entry.name}: {target_classes[int(entry.y_class[i])]}', fontsize=fontsize)
+            plt.grid(False)
+            plt.axis('off')
 
-        plt.subplot(n, 2, 2*i + 2)
-        plt.imshow(VTF.to_pil_image(x[i]))
-        plt.imshow(VTF.to_pil_image(heatmap_hat), alpha=alpha)
-        plt.title(f'predicted: {target_classes[int(y_class_hat[i])]}', fontsize=fontsize)
-        plt.grid(False)
-        plt.axis('off')
+            image = entry.mode.combine(x[i], entry.mask[i])
+            plt.imshow(VTF.to_pil_image(image))
 
     return fig
