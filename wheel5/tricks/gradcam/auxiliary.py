7e73a28ee16ba45301652923aea80f03e51fd839
Alexander Demidko <alexander@fluence.one>
2020-04-07 12:41:51 +0300

    Batched GradCAM and GradCAM++

--- wheel5/tricks/gradcam/auxiliary.py
+++ wheel5/tricks/gradcam/auxiliary.py
@@ -2,21 +2,16 @@ import torch
 from typing import Callable, Optional
 
 
-def logit_to_score(class_index: Optional[int] = None) -> Callable[..., torch.Tensor]:
+def logit_to_score(class_index: Optional[torch.Tensor] = None) -> Callable[..., torch.Tensor]:
     def fn(logit: torch.Tensor):
-        # logit shape: (1, C)
-
+        # logit shape: (N, C)
         assert len(logit.shape) == 2
-        assert logit.shape[0] == 1
 
         if class_index is None:
-            values, _ = logit.topk(1, dim=1, sorted=False)
-            score = values.squeeze(dim=1)
+            score, _ = logit.max(dim=1)
         else:
-            _, c = logit.shape
-            assert 0 <= class_index < c
-
-            score = logit[:, class_index]
+            score = logit.gather(dim=1, index=class_index.unsqueeze(dim=1))
+            score = score.squeeze(dim=1)
 
         return score
 
--- wheel5/tricks/gradcam/generator.py
+++ wheel5/tricks/gradcam/generator.py
@@ -1,8 +1,9 @@
+from typing import Optional, Callable
+
 import torch
+import torch.nn.functional as F
 from torch import nn
 from torch.utils.hooks import RemovableHandle
-import torch.nn.functional as F
-from typing import Optional, Callable, Tuple
 
 from wheel5.util import Closure
 
@@ -30,19 +31,25 @@ class GradCAMBase(object):
 
     @staticmethod
     def upsample(saliency_map: torch.Tensor, inter_mode: str, h: int, w: int) -> torch.Tensor:
-        saliency_map = saliency_map.unsqueeze(dim=1)  # shape: (1, 1, U, V)
+        # saliency_map - shape: (N, U, V)
+        n, u, v = saliency_map.shape
+
         align_corners = False if inter_mode in ['linear', 'bilinear', 'bicubic', 'trilinear'] else None
-        saliency_map = F.interpolate(saliency_map,  # shape: (N, 1, H, W)
+
+        saliency_map = saliency_map.unsqueeze(dim=1)                        # shape: (N, 1, U, V)
+        saliency_map = F.interpolate(saliency_map,                          # shape: (N, 1, H, W)
                                      size=(h, w),
                                      mode=inter_mode,
                                      align_corners=align_corners)
-        saliency_map = saliency_map.squeeze(dim=0).squeeze(dim=0)  # shape: (H, W)
+        saliency_map = saliency_map.squeeze(dim=1)                          # shape: (N, H, W)
 
-        saliency_min = saliency_map.min()
-        saliency_max = saliency_map.max()
+        saliency_max, _ = saliency_map.view(n, -1).max(dim=1)
+        saliency_min, _ = saliency_map.view(n, -1).min(dim=1)
 
-        saliency_map = (saliency_map - saliency_min) / (saliency_max - saliency_min)
-        return saliency_map
+        saliency_max = saliency_max.view(n, 1, 1)
+        saliency_min = saliency_min.view(n, 1, 1)
+
+        return (saliency_map - saliency_min) / (saliency_max - saliency_min)
 
     def init(self):
         assert not self.entered
@@ -82,36 +89,35 @@ class GradCAM(GradCAMBase):
         super(GradCAM, self).__init__(model, layer, score_fn)
 
     def generate(self, input: torch.Tensor, inter_mode: str = 'bilinear') -> torch.Tensor:
-        # input - shape: (1, F, H, W)
-
+        # input - shape: (N, F, H, W)
         assert len(input.shape) == 4
-        assert input.shape[0] == 1
 
         with torch.enable_grad():
             n, f, h, w = input.shape
 
-            output: torch.Tensor = self.model(input)                        # y - shape: (1, C)
+            output: torch.Tensor = self.model(input)                        # y - shape: (N, C)
             assert len(output.shape) == 2
-            assert output.shape[0] == 1
+            assert output.shape[0] == n
 
-            score = self.score_fn(output)                                   # y_c - scalar
-
+            score = self.score_fn(output)                                   # y_c - shape: (N)
             assert len(score.shape) == 1
             assert score.shape[0] == n
 
             self.model.zero_grad()
-            score.backward()
+            grad_seed = score.new_ones(score.shape)
+            score.backward(grad_seed)
 
-            act: torch.Tensor = self.activations_closure.value              # A - shape: (1, K, U, V)
-            grad: torch.Tensor = self.gradients_closure.value               # dy^c / dA - shape: (1, K, U, V)
+            act: torch.Tensor = self.activations_closure.value              # A - shape: (N, K, U, V)
+            grad: torch.Tensor = self.gradients_closure.value               # dy^c / dA - shape: (N, K, U, V)
 
         with torch.no_grad():
-            _, k, u, v = grad.shape
+            n2, k, u, v = grad.shape
+            assert n2 == n
 
-            alpha = grad.view(n, k, -1).mean(dim=2)                         # avg over UxV for each A_k - shape: (1, K)
-            weights = alpha.view(n, k, 1, 1)                                # α^c_k - shape: (1, K, 1, 1)
-            lin_comb = (weights * act).sum(dim=1)                           # shape: (1, U, V)
-            saliency_map = F.relu(lin_comb)                                 # L^c - shape: (1, U, V)
+            alpha = grad.view(n, k, -1).mean(dim=2)                         # avg over UxV for each A_k - shape: (N, K)
+            weights = alpha.view(n, k, 1, 1)                                # α^c_k - shape: (N, K, 1, 1)
+            lin_comb = (weights * act).sum(dim=1)                           # shape: (N, U, V)
+            saliency_map = F.relu(lin_comb)                                 # L^c - shape: (N, U, V)
 
             return self.upsample(saliency_map, inter_mode, h, w)
 
@@ -122,79 +128,40 @@ class GradCAMpp(GradCAMBase):
         super(GradCAMpp, self).__init__(model, layer, score_fn)
 
     def generate(self, input: torch.Tensor, inter_mode: str = 'bilinear') -> torch.Tensor:
-        # input - shape: (1, F, H, W)
-
+        # input - shape: (N, F, H, W)
         assert len(input.shape) == 4
-        assert input.shape[0] == 1
 
         with torch.enable_grad():
             n, f, h, w = input.shape
 
-            output: torch.Tensor = self.model(input)                        # S - shape: (1, C)
+            output: torch.Tensor = self.model(input)                        # S - shape: (N, C)
+
             assert len(output.shape) == 2
-            assert output.shape[0] == 1
+            assert output.shape[0] == n
 
-            score = self.score_fn(output)                                   # S^c - scalar
-
+            score = self.score_fn(output)                                   # S^c - shape: (N)
             assert len(score.shape) == 1
             assert score.shape[0] == n
 
             self.model.zero_grad()
-            score.backward()
+            grad_seed = score.new_ones(score.shape)
+            score.backward(grad_seed)
 
-            act: torch.Tensor = self.activations_closure.value              # A - shape: (1, K, U, V)
-            grad: torch.Tensor = self.gradients_closure.value               # dS^c / dA - shape: (1, K, U, V)
+            act: torch.Tensor = self.activations_closure.value              # A - shape: (N, K, U, V)
+            grad: torch.Tensor = self.gradients_closure.value               # dS^c / dA - shape: (N, K, U, V)
 
         with torch.no_grad():
             grad_2 = grad.pow(2)
             grad_3 = grad.pow(3)
 
-            alpha_numer = grad_2                                            # (dS^c / dA)**2 - shape: (1, K, U, V)
-            alpha_denom = grad_2 * 2 + (grad_3 * act).sum(dim=(2, 3), keepdim=True)
+            alpha_numer = grad_2                                            # (dS^c / dA)**2 - shape: (N, K, U, V)
+            alpha_denom_add = (grad_3 * act).sum(dim=(2, 3), keepdim=True)  # shape: (N, K, 1, 1)
+            alpha_denom = grad_2 * 2 + alpha_denom_add                      # shape: (N, K, U, V)
 
-            alpha = alpha_numer / (alpha_denom + 1e-7)                      # α^kc_ij - shape: (1, K, U, V)
-            score = score.exp() * grad                                      # dY^c / dA = exp(S^c) * dS^c / dA - shape: (1, K, U, V)
-            weights = alpha * F.relu(score).sum(dim=(2, 3), keepdim=True)   # shape: (1, K, 1, 1)
-            saliency_map = (weights * act).sum(dim=1)                       # L^c - shape: (1, U, V)
+            alpha = alpha_numer / (alpha_denom + 1e-7)                      # α^kc_ij - shape: (N, K, U, V)
+            score = score.exp().view(n, 1, 1, 1) * grad                     # dY^c / dA = exp(S^c) * dS^c / dA - shape: (N, K, U, V)
+            weights_pixel = alpha * F.relu(score)                           # shape: (N, K, U, V)
+            weights = weights_pixel.sum(dim=(2, 3), keepdim=True)           # shape: (N, K, 1, 1)
+            saliency_map = (weights * act).sum(dim=1)                       # L^c - shape: (N, U, V)
 
             return self.upsample(saliency_map, inter_mode, h, w)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
