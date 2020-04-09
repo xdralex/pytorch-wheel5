e75fcff68e6d3ac2e8edb7a8257cfa46cafae057
Alexander Demidko <alexander@fluence.one>
2020-04-09 12:19:44 +0300

    Added masked cutmix operation

--- wheel5/dataset/functional.py
+++ wheel5/dataset/functional.py
@@ -1,8 +1,8 @@
+from typing import Optional, Tuple, List
+
 import numpy as np
 import torch
 from numpy.random.mtrand import RandomState
-from torch import Tensor
-from typing import Optional, Tuple, List
 
 
 def class_distribution(targets: List[int], classes: int) -> np.ndarray:
@@ -16,32 +16,57 @@ def class_distribution(targets: List[int], classes: int) -> np.ndarray:
     return counts
 
 
-def mixup(img1: Tensor, lb1: Tensor,
-          img2: Tensor, lb2: Tensor,
+def mixup(img1: torch.Tensor, lb1: torch.Tensor,
+          img2: torch.Tensor, lb2: torch.Tensor,
           alpha: float,
-          random_state: Optional[RandomState] = None) -> Tuple[Tensor, Tensor]:
+          random_state: Optional[RandomState] = None) -> Tuple[torch.Tensor, torch.Tensor]:
     # img shape: (F, H, W)
     # lb shape: (C)
 
     random_state = random_state or RandomState()
     q = random_state.beta(a=alpha, b=alpha)
 
-    with torch.no_grad():
-        assert len(img1.shape) == 3 and len(img2.shape) == 3
-        assert len(lb1.shape) == 1 and len(lb2.shape) == 1
-        assert img1.shape == img2.shape
-        assert lb1.shape == lb2.shape
+    assert len(img1.shape) == 3 and len(img2.shape) == 3
+    assert len(lb1.shape) == 1 and len(lb2.shape) == 1
+    assert img1.shape == img2.shape
+    assert lb1.shape == lb2.shape
 
-        img = torch.lerp(img1, img2, weight=q)
-        lb = torch.lerp(lb1, lb2, weight=q)
+    img = torch.lerp(img1, img2, weight=q)
+    lb = torch.lerp(lb1, lb2, weight=q)
 
-        return img, lb
+    return img, lb
 
 
-def cutmix(img_src: Tensor, lb_src: Tensor,
-           img_dst: Tensor, lb_dst: Tensor,
+def masked_cutmix(img_src: torch.Tensor, lb_src: torch.Tensor,
+                  img_dst: torch.Tensor, lb_dst: torch.Tensor,
+                  mask_src: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    # img shape: (F, H, W)
+    # lb shape: (C)
+    # mask shape: (H, W)
+
+    assert len(img_src.shape) == 3 and len(img_dst.shape) == 3
+    assert len(lb_src.shape) == 1 and len(lb_dst.shape) == 1
+    assert len(mask_src.shape) == 2
+
+    assert img_src.shape == img_dst.shape
+    assert lb_src.shape == lb_dst.shape
+
+    assert img_src.shape[1:] == mask_src.shape
+
+    img = img_dst.new_zeros(img_dst.shape)
+    img[:, mask_src] = img_src[:, mask_src]
+    img[:, ~mask_src] = img_dst[:, ~mask_src]
+
+    weight_src = float(mask_src.int().sum()) / float(mask_src.numel())
+    lb = torch.lerp(lb_dst, lb_src, weight_src)
+
+    return img, lb
+
+
+def cutmix(img_src: torch.Tensor, lb_src: torch.Tensor,
+           img_dst: torch.Tensor, lb_dst: torch.Tensor,
            alpha: float, mode: str = 'compact',
-           random_state: Optional[RandomState] = None) -> Tuple[Tensor, Tensor]:
+           random_state: Optional[RandomState] = None) -> Tuple[torch.Tensor, torch.Tensor]:
     # img shape: (F, H, W)
     # lb shape: (C)
 
@@ -53,38 +78,37 @@ def cutmix(img_src: Tensor, lb_src: Tensor,
     if (mode == 'compact' and q > 0.5) or (mode == 'halo' and q < 0.5):
         q = 1 - q
 
-    with torch.no_grad():
-        assert len(img_src.shape) == 3 and len(img_dst.shape) == 3
-        assert len(lb_src.shape) == 1 and len(lb_dst.shape) == 1
-        assert img_src.shape[0] == img_dst.shape[0]
-        assert lb_src.shape == lb_dst.shape
+    assert len(img_src.shape) == 3 and len(img_dst.shape) == 3
+    assert len(lb_src.shape) == 1 and len(lb_dst.shape) == 1
+    assert img_src.shape[0] == img_dst.shape[0]
+    assert lb_src.shape == lb_dst.shape
 
-        src_h, src_w = img_src.shape[1:]
-        dst_h, dst_w = img_dst.shape[1:]
+    src_h, src_w = img_src.shape[1:]
+    dst_h, dst_w = img_dst.shape[1:]
 
-        factor = np.sqrt(q)
-        patch_h, patch_w = int(np.round(dst_h * factor)), int(np.round(dst_w * factor))
-        patch_h, patch_w = min(patch_h, src_h), min(patch_w, src_w)
+    factor = np.sqrt(q)
+    patch_h, patch_w = int(np.round(dst_h * factor)), int(np.round(dst_w * factor))
+    patch_h, patch_w = min(patch_h, src_h), min(patch_w, src_w)
 
-        dst_cy = random_state.randint(dst_h)
-        dst_cx = random_state.randint(dst_w)
+    dst_cy = random_state.randint(dst_h)
+    dst_cx = random_state.randint(dst_w)
 
-        dst_y1 = int(np.clip(dst_cy - patch_h // 2, 0, dst_h))
-        dst_y2 = int(np.clip(dst_cy + patch_h // 2, 0, dst_h))
-        dst_x1 = int(np.clip(dst_cx - patch_w // 2, 0, dst_w))
-        dst_x2 = int(np.clip(dst_cx + patch_w // 2, 0, dst_w))
+    dst_y1 = int(np.clip(dst_cy - patch_h // 2, 0, dst_h))
+    dst_y2 = int(np.clip(dst_cy + patch_h // 2, 0, dst_h))
+    dst_x1 = int(np.clip(dst_cx - patch_w // 2, 0, dst_w))
+    dst_x2 = int(np.clip(dst_cx + patch_w // 2, 0, dst_w))
 
-        patch_h, patch_w = dst_y2 - dst_y1, dst_x2 - dst_x1
+    patch_h, patch_w = dst_y2 - dst_y1, dst_x2 - dst_x1
 
-        src_y1 = random_state.randint(src_h - patch_h + 1)
-        src_y2 = src_y1 + patch_h
-        src_x1 = random_state.randint(src_w - patch_w + 1)
-        src_x2 = src_x1 + patch_w
+    src_y1 = random_state.randint(src_h - patch_h + 1)
+    src_y2 = src_y1 + patch_h
+    src_x1 = random_state.randint(src_w - patch_w + 1)
+    src_x2 = src_x1 + patch_w
 
-        img = img_dst.clone()
-        img[:, dst_y1:dst_y2, dst_x1:dst_x2] = img_src[:, src_y1:src_y2, src_x1:src_x2]
+    img = img_dst.clone()
+    img[:, dst_y1:dst_y2, dst_x1:dst_x2] = img_src[:, src_y1:src_y2, src_x1:src_x2]
 
-        weight = float(patch_h * patch_w) / float(dst_h * dst_w)
-        lb = torch.lerp(lb_dst, lb_src, weight=weight)
+    weight = float(patch_h * patch_w) / float(dst_h * dst_w)
+    lb = torch.lerp(lb_dst, lb_src, weight=weight)
 
-        return img, lb
+    return img, lb
