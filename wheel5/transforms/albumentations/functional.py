f86fc6736f4d1a834b9ef073c0e693be8b1809c0
Alexander Demidko <alexander@fluence.one>
2020-04-09 12:18:16 +0300

    Removed obsolete resize operation

--- wheel5/transforms/albumentations/functional.py
+++ wheel5/transforms/albumentations/functional.py
@@ -2,10 +2,7 @@ from numbers import Number
 from typing import Union
 
 import cv2
-import numpy as np
-from PIL import Image
 from albumentations.augmentations import functional as AF
-from torchvision.transforms import functional as TF
 
 
 def pad_to_square(img, fill: Union[Number, str, tuple] = 0):
@@ -26,9 +23,3 @@ def pad_to_square(img, fill: Union[Number, str, tuple] = 0):
                                  value=fill)
 
     return img
-
-
-def resize(img, height: int, width: int, interpolation: int = Image.BILINEAR):
-    img = Image.fromarray(img)
-    img = TF.resize(img, (height, width), interpolation)
-    return np.array(img)
--- wheel5/transforms/albumentations/pipeline.py
+++ wheel5/transforms/albumentations/pipeline.py
@@ -1,10 +1,9 @@
 from numbers import Number
 from typing import Union
 
-from PIL import Image
 from albumentations import ImageOnlyTransform
 
-from .functional import pad_to_square, resize
+from .functional import pad_to_square
 
 
 class PadToSquare(ImageOnlyTransform):
@@ -19,25 +18,6 @@ class PadToSquare(ImageOnlyTransform):
 
     def get_transform_init_args_names(self):
         return "fill",
-
-    def get_params_dependent_on_targets(self, params):
-        raise NotImplementedError()
-
-
-class Resize(ImageOnlyTransform):
-    def __init__(self, height: int, width: int, interpolation: int = Image.BILINEAR,
-                 always_apply=False, p=1.0):
-        super(Resize, self).__init__(always_apply, p)
-
-        self.height = height
-        self.width = width
-        self.interpolation = interpolation
-
-    def apply(self, img, **params):
-        return resize(img, self.height, self.width, self.interpolation)
-
-    def get_transform_init_args_names(self):
-        return "height", "width", "interpolation"
 
     def get_params_dependent_on_targets(self, params):
         raise NotImplementedError()
